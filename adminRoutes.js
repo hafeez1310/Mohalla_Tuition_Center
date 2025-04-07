213d04f3cc6ffc52f382fb363c078bb77d014da3
hafeez1310 <mohammedhafeez13102004@gmail.com>
2025-04-07 16:15:45 +0530

    Add files via upload

---
+++ adminRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { addCenter, addTutor, getCenters, getTutors, getStudents, getAttendanceSummary } = require('../controllers/adminController');
+const auth = require('../middleware/authMiddleware');
+const multer = require('multer');
+const adminController = require('../controllers/adminController')
+// const { addStudent } = require('../controllers/adminController');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+});
+const upload = multer({ storage });
+
+router.post('/center', auth(['admin']), upload.single('image'), addCenter);
+router.post('/tutor', auth(['admin']), upload.single('resume'), addTutor);
+router.post('/students', adminController.addStudent);
+router.get('/centers', auth(['admin']), getCenters);
+router.get('/tutors', auth(['admin']), getTutors);
+router.get('/students', auth(['admin']), getStudents);
+router.get('/attendance-summary', auth(['admin']), getAttendanceSummary);
+
+module.exports = router;
